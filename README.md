82dfce5b279843a50fc1494a20ee86679db35cf4
lblock181 <lblockfound@gmail.com>
2023-12-08 22:06:55 -0600

    init upload. need to add day 1 from other machine

--- README.md
+++ README.md
@@ -1,2 +1,65 @@
 # adventofcode
 Advent of Code 2023 Challenges. Done when I have time
+
+Day 1
+    - Done but needs to be uploaded
+Day 2
+    - Done & uploaded
+Day 3
+    - TBD
+Day 4
+    - TBD
+Day 5
+    - TBD
+Day 6
+    - TBD
+Day 7
+    - TBD
+Day 8
+    - TBD
+Day 9
+    - TBD
+Day 10
+    - TBD
+Day 11
+    - TBD
+Day 12
+    - TBD
+Day 13
+    - TBD
+Day 14
+    - TBD
+Day 15
+    - TBD
+Day 16
+    - TBD
+Day 17
+    - TBD
+Day 18
+    - TBD
+Day 19
+    - TBD
+Day 20
+    - TBD
+Day 21
+    - TBD
+Day 22
+    - TBD
+Day 23
+    - TBD
+Day 24
+    - TBD
+Day 25
+    - TBD
+Day 26
+    - TBD
+Day 27
+    - TBD
+Day 28
+    - TBD
+Day 29
+    - TBD
+Day 30
+    - TBD
+Day 31
+    - TBD
---
+++ day2/solution.py
@@ -0,0 +1,86 @@
+dirpath = "C:/Users/lbloc/Documents/AdventofCode/day2/"
+# filename = "ex1.txt"
+# filename = "ex2.txt"
+# filename = "input1.txt"
+filename = "input2.txt"
+
+
+def sol2():
+    with open(dirpath + filename, 'r') as f:
+        contents:list[str] = f.readlines()
+
+    valid_games: list[int] = []
+
+    for line in [ x.strip().replace("\n","") for x in contents ]:
+        game_id, games = line.split(":")
+        game_id = int(game_id.split(" ")[1])
+        
+        min_red = -1
+        min_green = -1
+        min_blue = -1
+        sets:list[str] = games.split(";")
+        for s in sets:
+            cubes = s.strip().split(",")
+            for c in cubes:
+                val, color = c.strip().split(" ")
+                val = int(val)
+                match color:
+                    case "red":
+                        if min_red == -1 or val > min_red:
+                            min_red = val
+                    case "green":
+                        if min_green == -1 or val > min_green:
+                            min_green = val
+                    case "blue":
+                        if min_blue == -1 or val > min_blue:
+                            min_blue = val
+        valid_games.append((min_red * min_blue * min_green))
+        
+
+    solution = sum(valid_games)
+
+    print(solution)
+
+
+def sol1():
+    max_red = 12
+    max_green = 13
+    max_blue = 14
+    with open(dirpath + filename, 'r') as f:
+        contents:list[str] = f.readlines()
+
+    valid_games: list[int] = []
+
+    for line in [ x.strip().replace("\n","") for x in contents ]:
+        game_id, games = line.split(":")
+        game_id = int(game_id.split(" ")[1])
+        
+        sets:list[str] = games.split(";")
+        game_possible: bool = True
+        for s in sets:
+            cubes = s.strip().split(",")
+            for c in cubes:
+                val, color = c.strip().split(" ")
+                val = int(val)
+                match color:
+                    case "red":
+                        if val > max_red:
+                            game_possible = False
+                    case "green":
+                        if val > max_green:
+                            game_possible = False
+                    case "blue":
+                        if val > max_blue:
+                            game_possible = False
+                if not game_possible:
+                    break
+        if game_possible:
+            valid_games.append(game_id)
+
+    solution = sum(valid_games)
+
+    print(solution)
+
+if __name__ == "__main__":
+    # sol1()
+    sol2()
