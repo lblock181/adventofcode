4378f9fec9879332b174db9766f7ece1f05b9be7
lblock181 <lblockfound@gmail.com>
2023-12-10 21:27:21 -0600

    finished day3 part 2

--- day3/solution.py
+++ day3/solution.py
@@ -1,8 +1,8 @@
 import os
 
 dirpath = os.path.dirname(os.path.abspath(__file__)) + "\\"
-filename = "ex1.txt"
-# filename = "ex2.txt"
+# filename = "ex1.txt"
+filename = "ex2.txt"
 filename = "input.txt"
 
 def get_file_contents() -> list[str]:
@@ -35,7 +35,32 @@ def sol1():
 
 def sol2():
     contents = get_file_contents()
+    solution = []
+    for r_ind, line in enumerate(contents):
+        for char_ind, ch in enumerate(line):
+            if ch != "*":
+                continue
+            coord_set = set()
+            for curr_row in [r_ind - 1, r_ind, r_ind + 1]:
+                for curr_col in [char_ind - 1, char_ind, char_ind + 1]:
+                    if curr_row < 0 or curr_row >= len(contents) or curr_col < 0 or curr_col >= len(contents[curr_row]) or not contents[curr_row][curr_col].isdigit():
+                        continue
+                    while curr_col > 0 and contents[curr_row][curr_col - 1].isdigit():
+                        curr_col -= 1
+                    coord_set.add((curr_row, curr_col))
+            if len(coord_set) != 2:
+                continue
+            temp_sol = [] 
+            for row, col in coord_set:
+                temp_str = ""
+                while col < len(contents[row]) and contents[row][col].isdigit():
+                    temp_str += contents[row][col]
+                    col += 1
+                temp_sol.append(int(temp_str))
+            solution.append(temp_sol[0] * temp_sol[1])
+    
+    print(sum(solution))
 
 if __name__ == "__main__":
-    sol1()
-    # sol2()
+    # sol1()
+    sol2()
